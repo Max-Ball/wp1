20afd3a229b923d501a270e82232a55c0a6df5dd
Max Ball <max.ball3388@gmail.com>
2022-11-11 17:33:43 -0700

    sasswatch

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -8,29 +9,44 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
-$navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
+$container = get_theme_mod('understrap_container_type')
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
+	<?php do_action('wp_body_open'); ?>
+	<div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<header id="wrapper-navbar">
+		<!-- ******************* The Navbar Area ******************* -->
+		<div id="wrapper-navbar">
+			<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-primary" aria-labelledby="main-nav-label">
+				<div class="container">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'primary menu',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id' => 'navbarNavDropdown',
+							'menu_class' => 'navbar-nav ml-auto',
+							'fallback_cb' => '',
+							'menu_id' => 'main-menu',
+							'depth' => 2,
+							'walker' => new Understrap_WP_Bootstrap_Navwalker(),
+						)
+					);
+					?>
+				</div>
+			</nav>
+		</div>
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-
-		<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
-
-	</header><!-- #wrapper-navbar end -->
+		<!-- #wrapper-navbar end -->
